ec643c81d897357e3d1eb0954b2d9f2300acae17
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2023-11-28 10:56:32 -0600

    initial commit - hello world

---
+++ 001-variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  final String pokemon = 'Ditto';
+  final int hp = 48;
+
+  print("""
+      $pokemon
+      $hp
+  """); // la tabulación se nota en la consola
+
+  print('Hello word');
+}
